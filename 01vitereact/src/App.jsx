b3ae5641ad0245555180071446879c3cbf0465d9
Anil Pal <anilcodes01@gmail.com>
2024-04-01 10:53:31 +0530

    Trying react for first time

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+import Random from "./index";
+
+function App() {
+  return (
+    <>
+      <Random />
+      <h3>Go fuck yourself!</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        molestias, ut dicta officiis inventore dolorem recusandae omnis rerum
+        eveniet itaque esse dignissimos! Illo dolores, cupiditate deserunt
+        corporis repellat soluta dolore!
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/index.jsx
@@ -0,0 +1,7 @@
+function Random() {
+    return(
+        <h3>This is just a trial!</h3>
+    )
+}
+
+export default Random
